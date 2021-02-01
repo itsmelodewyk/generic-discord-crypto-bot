8ccf98a6c98b18e3de0f0e8ae2bdba4906678df8
Lodewyk van der Westhuizen <itsmelodewyk@gmail.com>
2021-01-31 19:45:28 -0600

    Add percentage of total supply in circulation
    Change variable "stats" to "metrics" and indent multi-line string. Add percentage of total supply currently in circulation.

--- index.js
+++ index.js
@@ -26,18 +26,24 @@ cron.schedule('*/5 * * * *', () => {
   queryCoinGecko();
 })
 
+function round(value, precision) {
+    const multiplier = 10 ** (precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
+
 function queryCoinGecko() {
   axios.get(COINGECKO_URL)
     .then(response => {
       try {
-        var stats =
-        COIN_TICKER + "/USD: $" + response.data.market_data.current_price.usd + "\n" +
-        COIN_TICKER + "/EUR: €" + response.data.market_data.current_price.eur + "\n" +
-        "Market Cap Rank: " + response.data.market_cap_rank + "\n" +
-        "Circulating Supply/Total Supply : " + response.data.market_data.circulating_supply + "/" + response.data.market_data.total_supply
+        var percentageCircSupply = round(((response.data.market_data.circulating_supply / response.data.market_data.total_supply) * 100), 1);
+        var metrics =
+          COIN_TICKER + "/USD: $" + response.data.market_data.current_price.usd + "\n" +
+          COIN_TICKER + "/EUR: €" + response.data.market_data.current_price.eur + "\n" +
+          "Market Cap Rank: " + response.data.market_cap_rank + "\n" +
+          "Circulating Supply: " + percentageCircSupply + "% (" + round(response.data.market_data.circulating_supply, 0) + "/" + round(response.data.market_data.total_supply, 0) + ")";
 
         // send update to CHANNEL_ID
-        bot.channels.get(CHANNEL_ID).send(stats);
+        bot.channels.get(CHANNEL_ID).send(metrics);
       } catch (error) {
         console.log(error);
       }
